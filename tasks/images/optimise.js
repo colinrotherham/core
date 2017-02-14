34548297716699fcc8c24e5da968651945c443a7
Colin Rotherham <work@colinr.com>
2017-02-14 11:07:27 +0000

    Tweak image optimiser defaults
    Switch to mozjpeg etc

--- tasks/images/optimise.js
+++ tasks/images/optimise.js
@@ -8,16 +8,28 @@ module.exports = function (paths, gulp, plugins) {
 
 	// Child modules
 	var pngquant = require('imagemin-pngquant');
+	var mozjpeg = require('imagemin-mozjpeg');
 
 	// Return module
 	return function () {
 
+		// Override plugins (default + pngquant, mozjpeg)
+		var use = [
+			plugins.imagemin.gifsicle(),
+			plugins.imagemin.svgo(),
+			pngquant(),
+			mozjpeg({
+				quality: 70,
+				progressive: true
+			})
+		];
+
+		// Additional options
 		var options = {
-			progressive: true,
-			use: [pngquant()]
+			progressive: true
 		};
 
-		return gulp.src(plugins.path.resolve(paths.build, '**/*.{png,jpg,gif}'))
+		return gulp.src(plugins.path.resolve(paths.build, '**/*.{png,jpg,gif,svg}'))
 			.pipe(plugins.imagemin(options))
 			.pipe(gulp.dest(plugins.path.resolve(paths.build, 'assets/img')));
 	};
