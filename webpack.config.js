e0d75e3a121b5c2d07180a10406b55dac13b5343
Colin Rotherham <work@colinr.com>
2017-06-29 22:15:52 +0100

    Apply Babel settings for IE8 compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,11 @@ export default {
 							useBuiltIns: 'usage',
 							targets: { browsers: browserslist }
 						}]
+					],
+					plugins: [
+						['transform-es2015-modules-commonjs', { loose: true }],
+						'transform-es3-member-expression-literals',
+						'transform-es3-property-literals'
 					]
 				}
 			}]
