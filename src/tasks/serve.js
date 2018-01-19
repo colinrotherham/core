4994bcfd2384c7f188dcc5fd1182d8fab935f102
Colin Rotherham <work@colinr.com>
2018-01-19 23:23:39 +0000

    Move Browsersync server config to defaults
    For proxied sites, it shouldn’t be set

--- src/tasks/serve.js
+++ src/tasks/serve.js
@@ -11,11 +11,11 @@ export default config => {
 		notify: false,
 		open: false,
 		reloadDelay: 400,
-		reloadThrottle: 100
+		reloadThrottle: 100,
+		server: {
+			baseDir: config.src
+		}
 	};
-
-	options.server = options.server || {};
-	options.server.baseDir = config.src;
 
 	return () => browserSync(options);
 };
